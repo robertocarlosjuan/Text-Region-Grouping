ade78b4bdb69e6cf002876fa21dbba704f824bb3
robertocarlosjuan <carlos333e@gmail.com>
2022-05-11 16:30:39 +0800

    updated for channels, titles, rolling capitons, scene text

--- rules.py
+++ rules.py
@@ -1,22 +1,87 @@
 import json
 import os
-from utils import levenshteinDistance, check_bbox_at_edge, parse_args, processImages, detect_bounding, get_bbox_location_dict, draw_bounding
-from config import iou_threshold, edge_threshold, full_video_threshold, edit_distance_leeway
+from utils import *
+from config import *
 
-# python rules.py --video_path "/home/hcari/trg/videos/"
+# python rules.py --video_path "/home/hcari/trg/videos/" --vis_dir /home/hcari/trg/vis/
 
-def find_channel(bbox_loc, num_frames, height, width, edge_threshold, full_video_threshold=0.7, edit_leeway=3):
+def process(bbox_loc, num_frames, height, width, edge_threshold, full_video_threshold=0.7, edit_sim=0.7, edit_sim_rolling_caption=0.5, hor_leeway=1, rc_height_thres=0.15):
     # threshold: proportion of frames that have the bbox
     channels = []
-    for bbox, pred_texts in bbox_loc.items():
-        # Check if bbox last throughout video
-        if len(pred_texts)/num_frames > full_video_threshold:
-            # Check if texts are the same
-            if all(levenshteinDistance(x, pred_texts[0])<=edit_leeway for x in pred_texts):
-                # Check if bbox at edges
-                if check_bbox_at_edge(bbox, height, width, edge_threshold):
-                    channels.append((json.loads(bbox), pred_texts[0]))
-    return channels
+    titles = []
+    rolling_captions = []
+    scene_texts = []
+    for bbox, pred_dict in bbox_loc.items():
+        pred_texts = [x[0] for x in pred_dict if x[1]=="iou"]
+        rc_pred_texts = [x[0] for x in pred_dict if x[1]=="vert_iou"]
+        combined = [x[0] for x in pred_dict]
+        print(combined[0])
+        print(bbox)
+        # Check bbox_horizontal
+        if check_horizontal(json.loads(bbox), threshold = hor_leeway):
+            print("is_horizontal")
+            # Check if bbox last throughout video
+            if len(combined)/num_frames > full_video_threshold:
+                print("combined last through video")
+                edit_similarities, avg_edit_sim, prop_unzero = get_edit_similarities(pred_texts)
+                print(edit_similarities)
+                print("avg_edit_sim: ", avg_edit_sim)
+                print("prop_unzero: ", prop_unzero)
+                if len(pred_texts)/num_frames > full_video_threshold:
+                    print("pred_text last through video")
+                    # Check if texts are the same
+                    if prop_unzero>edit_sim: #avg_edit_sim >= edit_sim
+                        print("Same text across frames")
+                        # Check if bbox at edges
+                        if check_bbox_at_edge(bbox, height, width, edge_threshold):
+                            print("Bounding box at the edge")
+                            channels.append((json.loads(bbox), pred_texts[0]))
+                            print("channels\n")
+                        else:
+                            print("Bounding box not at the edge")
+                            titles.append((json.loads(bbox), pred_texts[0]))
+                            print("titles\n")
+                    else:
+                        # Check if rolling captions
+                        # Check if text in next frame similar to this frame
+                        # Check that bbox is in bottom area of frame
+                        print("Texts are not always the same")
+                        below = check_bbox_below(bbox, height, threshold = rc_height_thres)
+                        is_rc_text = maybe_is_rc(rc_pred_texts)
+                        print("rc_pred_text: ", rc_pred_texts)
+                        print("Below: ", below)
+                        print("is_rc_text: ", is_rc_text)
+                        if below and is_rc_text:
+                            rolling_captions.append((json.loads(bbox), rc_pred_texts[0]))
+                            print("rolling caption\n")
+                        else:
+                            scene_texts.append((json.loads(bbox), combined[0]))
+                            print("scene_text\n")
+                else:
+                    if prop_unzero>edit_sim: #avg_edit_sim >= edit_sim
+                        titles.append((json.loads(bbox), pred_texts[0]))
+                        print("titles\n")
+                    else:
+                        print("mostly vert_iou: ", len(rc_pred_texts)/num_frames > full_video_threshold)
+                        below = check_bbox_below(bbox, height, threshold = rc_height_thres)
+                        is_rc_text = maybe_is_rc(rc_pred_texts)
+                        print("rc_pred_text: ", rc_pred_texts)
+                        print("Below: ", below)
+                        print("is_rc_text: ", is_rc_text)
+                        if below and is_rc_text:
+                            rolling_captions.append((json.loads(bbox), rc_pred_texts[0]))
+                            print("rolling caption\n")
+                        else:
+                            scene_texts.append((json.loads(bbox), combined[0]))
+                            print("scene_text\n")
+            else:
+                scene_texts.append((json.loads(bbox), combined[0]))
+                print("scene_text\n")
+        else:
+            print("not horizontal")
+            scene_texts.append((json.loads(bbox), combined[0]))
+            print("scene_text\n")
+    return channels, titles, rolling_captions, scene_texts
 
 def main():
     args = parse_args()
@@ -25,12 +90,21 @@ def main():
     for video_path in video_paths:
         print(video_path)
         results, height, width, output_path = processImages(video_path, detect_bounding, image_dir=args.vis_dir, num_frames_to_save=1)
-        print(results)
-        bbox_loc, num_frames = get_bbox_location_dict(results, threshold=iou_threshold)
+        bbox_loc, num_frames = get_bbox_location_dict(results, threshold=iou_threshold, rc_iou=rc_iou)
         print("NUM FRAMES: ", num_frames)
-        print(bbox_loc)
-        channels = find_channel(bbox_loc, num_frames, height, width, edge_threshold=edge_threshold, full_video_threshold=full_video_threshold, edit_leeway=edit_distance_leeway)
-        print(channels)
-        draw_bounding(output_path, channels, output_dir=None)
+        channels, titles, rolling_captions, scene_texts = process(
+                                                            bbox_loc, 
+                                                            num_frames, 
+                                                            height, width, 
+                                                            edge_threshold=edge_threshold, 
+                                                            full_video_threshold=full_video_threshold, 
+                                                            edit_sim=edit_sim, 
+                                                            edit_sim_rolling_caption=edit_sim_rolling_caption,
+                                                            hor_leeway=hor_leeway, 
+                                                            rc_height_thres=rc_height_thres)
+        draw_bounding(output_path, channels, output_dir=args.vis_dir, label="_channels")
+        draw_bounding(output_path, titles, output_dir=args.vis_dir, label="_titles")
+        draw_bounding(output_path, rolling_captions, output_dir=args.vis_dir, label="_roling_captions")
+        draw_bounding(output_path, scene_texts, output_dir=args.vis_dir, label="_scene_texts")
 
 main()
--- utils.py
+++ utils.py
@@ -1,16 +1,15 @@
+from lib2to3.pgen2.literals import simple_escapes
 import os
 import json
 import math
 import argparse
+import statistics
 import numpy as np
 import cv2
-print(cv2.__version__)
 from PIL import Image
 from config import ocr, font_path, sample_rate
 from paddleocr import draw_ocr
 from shapely.geometry import Polygon
-
-# python utils.py --video_path "/home/hcari/trg/videos/ChineseSinovacVideo.mp4"
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -19,7 +18,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def get_bbox_location_dict(results, threshold=0.5):
+def get_bbox_location_dict(results, threshold=0.5, rc_iou=0.5):
     # bbox_location {bbox_coords: [rec1, rec2, ...]}
     bbox_loc = {}
     num_frames = 0
@@ -27,17 +26,24 @@ def get_bbox_location_dict(results, threshold=0.5):
         for bbox_info in frame:
             bbox = json.dumps(bbox_info[0])
             # Check if bbox is in previous frames
-            bbox_in_prev_frames = bbox if bbox in bbox_loc.keys() else None
+            bbox_in_prev_frames = ("iou",bbox) if bbox in bbox_loc.keys() else None
             if bbox_in_prev_frames is None:
                 for key in bbox_loc.keys():
                     iou = get_intersection_over_union(bbox_info[0], json.loads(key))
-                    if iou > threshold:
-                        bbox_in_prev_frames = key
+                    if iou > threshold: # Channel, title
+                        bbox_in_prev_frames = ("iou", key)
                         break
+                if bbox_in_prev_frames is None:
+                    for key in bbox_loc.keys():
+                        vert_iou = get_vertical_iou(bbox_info[0], json.loads(key)) # Rolling Captions
+                        if vert_iou > rc_iou:
+                            bbox_in_prev_frames = ("vert_iou", key)
+                            break
+
             if bbox_in_prev_frames is not None:
-                bbox_loc[bbox_in_prev_frames].append(bbox_info[1])
+                bbox_loc[bbox_in_prev_frames[1]].append((bbox_info[1], bbox_in_prev_frames[0]))
             else:
-                bbox_loc[bbox] = [bbox_info[1]]
+                bbox_loc[bbox] = [(bbox_info[1], "iou")]
         if len(frame) > 0:
             num_frames += 1
     return bbox_loc, num_frames    
@@ -77,7 +83,7 @@ def detect_bounding(img_name):
     ocr_bboxes = ocr.ocr(img_name, cls=True)
     return ocr_bboxes
 
-def draw_bounding(img_path, ocr_bboxes, output_dir=None):
+def draw_bounding(img_path, ocr_bboxes, output_dir=None, label="_bbox"):
     # draw bouding boxes on image
     image = Image.open(img_path).convert('RGB')
     boxes = [line[0] for line in ocr_bboxes]
@@ -85,11 +91,30 @@ def draw_bounding(img_path, ocr_bboxes, output_dir=None):
     scores = [line[1][1] for line in ocr_bboxes]
     im_show = draw_ocr(image, boxes, txts, scores, font_path=font_path)
     im_show = Image.fromarray(im_show)
-    output_path = "_bbox".join(os.path.splitext(img_path))
-    output_path = output_path if output_dir is None else os.path.join(output_dir, os.path.basename(output_path))
+    output_path = label.join(os.path.splitext(img_path))
+    if output_dir is None:
+        output_path = output_path  
+    elif os.path.isdir(output_dir):
+        output_path = os.path.join(output_dir, os.path.basename(output_path))
+    elif output_dir.endswith(".jpg") or output_dir.endswith(".jpeg"):
+        output_path = output_dir
     im_show.save(output_path)
 
 # Find Channel
+def get_vertical_iou(pD, pG):
+    min_pD = min([x[1] for x in pD])
+    max_pD = max([x[1] for x in pD])
+    min_pG = min([x[1] for x in pG])
+    max_pG = max([x[1] for x in pG])
+    min_union = min_pD if min_pD < min_pG else min_pG
+    min_inter = min_pG if min_pD < min_pG else min_pD
+    max_union = max_pD if max_pD > max_pG else max_pG
+    max_inter = max_pG if max_pD > max_pG else max_pD
+    if max_inter < min_inter:
+        return 0
+    else:
+        return (max_inter-min_inter) / (max_union-min_union)
+
 def get_intersection_over_union(pD, pG):
     return Polygon(pD).intersection(Polygon(pG)).area / Polygon(pD).union(Polygon(pG)).area
 
@@ -106,23 +131,71 @@ def levenshteinDistance(s1, s2):
             else:
                 distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
         distances = distances_
-    return distances[-1]
+    return distances
 
-# def check_bbox_at_edge(bbox, height, width, threshold=0.15):
-#     if type(bbox) == str:
-#         bbox = json.loads(bbox)
-#     for edge in bbox:
-#         is_edge = [False, False]
-#         if edge[0] < threshold*width or edge[0] > (1-threshold)*width:
-#             is_edge[0] = True
-#         if edge[1] < threshold*height or edge[1] > (1-threshold)*height:
-#             is_edge[1] = True
-#         if not (is_edge[0] and is_edge[1]):
-#             return False
-#     return True      
+def norm_edit_sim(s1, s2):
+    s1 = s1[0] if type(s1)==tuple else s1
+    s2 = s2[0] if type(s2)==tuple else s2
+    m = len(s1) if len(s1) < len(s2) else len(s2)
+    d = levenshteinDistance(s1, s2)[-1]
+    if d>=m:
+        return 0
+    else:
+        return 1.0 / math.exp( d / (m - d) )
+
+def shortened_norm_edit_sim(s1, s2):
+    s1 = s1[0] if type(s1)==tuple else s1
+    s2 = s2[0] if type(s2)==tuple else s2
+    shorter = len(s1) if len(s1) < len(s2) else len(s2)
+    cut_off = math.ceil(shorter*0.8)
+    return norm_edit_sim(s1[cut_off*-1:], s2[:cut_off])
+
+# def maybe_is_rc(pred_texts, rc_text_thres=0.5):
+#     is_rc = 0
+#     not_rc = 0
+#     for i in range(len(pred_texts)-1):
+#         sim = norm_edit_sim(pred_texts[i][0], pred_texts[i+1][0])
+#         half_sim = shortened_norm_edit_sim(pred_texts[i][0], pred_texts[i+1][0])
+#         if half_sim > sim:
+#             is_rc += 1
+#         else:
+#             not_rc += 1
+#     norm_rc_likelihood = is_rc / (is_rc + not_rc)
+#     return True if norm_rc_likelihood > rc_text_thres else False
+
+def check_in(s1, s2):
+    s2 = s2[:len(s1)]
+    return norm_edit_sim(s1, s2) > 0.5
+
+# Check for clusters of text
+def maybe_is_rc(rc):
+    rc_sorted = sorted([x[0] for x in rc], key=len)
+    clusters = {}
+    for item in rc_sorted:
+        item_in_clusters = False
+        for key in clusters.keys():
+            if check_in(item, key):
+                clusters[key] += 1
+                item_in_clusters = True
+                break
+        if not item_in_clusters:
+            for i in range(len(rc_sorted)-1,0,-1):
+                key = rc_sorted[i]
+                if check_in(item, key):
+                    clusters[key] = 1
+                    break
+    refined_clusters = [(k,v) for k, v in clusters.items() if v>1 and v!=max(clusters.values())]
+    return len(refined_clusters)>0
+
+def check_bbox_below(bbox, height, threshold=0.15):
+    if type(bbox) == str:
+        bbox = json.loads(bbox)
+    if all(edge[1] < threshold*height for edge in bbox) or all(edge[1] > (1-threshold)*height for edge in bbox):
+        return True
+    else:
+        return False
 
 def check_bbox_at_edge(bbox, height, width, threshold=0.15):
-    print("HEIGHT WIDTH", height, width)
     if type(bbox) == str:
         bbox = json.loads(bbox)
     quad = [None,None]
@@ -155,16 +228,35 @@ def check_bbox_at_edge(bbox, height, width, threshold=0.15):
             edge_quad[1] = "bottom"
         if quad[0] is None or quad[1] is None:
             return False
-        if find_quad:
-            print("QUAD", quad)
         if edge_quad[0] is None or edge_quad[1] is None:
             return False
-        print("EDGE", edge_quad)
         find_quad = False
-    print(bbox)
     return True
-        
 
+def reject_outliers(data, m=5):
+    data = np.array(data)
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d/mdev if mdev else 0.
+    return np.squeeze(data[s<m]).tolist()
+
+def get_edit_similarities(pred_texts):
+    edit_similarities = []
+    for i in range(len(pred_texts)-1):
+        edit_similarity = norm_edit_sim(pred_texts[i][0], pred_texts[i+1][0])
+        edit_similarities.append(edit_similarity)
+    
+    edit_similarities = reject_outliers(edit_similarities)
+    # print(edit_similarities)
+    avg_edit_sim = sum(edit_similarities)/len(edit_similarities)
+    st_dev = statistics.stdev(edit_similarities)
+    prop_zero = edit_similarities.count(0) / len(edit_similarities)
+    prop_unzero = 1-prop_zero
+    print(pred_texts[0][0], avg_edit_sim, st_dev, prop_unzero)
+    return edit_similarities, avg_edit_sim, prop_unzero
+        
+def check_horizontal(bbox, threshold = 10):
+    return abs(bbox[0][1] - bbox[1][1]) <= threshold and abs(bbox[2][1] - bbox[3][1]) <= threshold
 
 
 
